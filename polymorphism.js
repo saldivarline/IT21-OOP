eafabb36e3c5dc79f761016dc71afc21f04d7644
aivan123504 <20232309@nbsc.edu.ph>
2024-10-11 10:16:14 +0800

    created polymorphism

---
+++ polymorphism.js
@@ -0,0 +1,30 @@
+class Person{
+    constructor(name){
+         this.name = name;
+     }
+
+     sayName(){
+         console.log(this.name);
+       }
+}
+
+class Student extends Person{
+   constructor(name, rollNumber){
+      super(name);
+      this.rollNumber = rollNumber;
+  }
+
+      logDetails(){
+      console.log(`Name: ${this.name}, Roll
+      number: ${this.rollNumber}`)
+      }
+
+      sayName(){
+      console.log("From Student");
+      super.sayName();
+      }
+
+}
+      var student = new Student("Sander", 1);
+      student.logDetails();
+      student.sayName();
